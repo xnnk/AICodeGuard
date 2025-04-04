a87f39479cfee1b195af6966d3fa9c059508989f
lzx <2360095286@qq.com>
2025-04-04 14:08:16 +0800

    init

---
+++ src/main/java/com/ai/aicodeguard/AiCodeGuardApplication.java
@@ -0,0 +1,13 @@
+package com.ai.aicodeguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCodeGuardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiCodeGuardApplication.class, args);
+    }
+
+}
